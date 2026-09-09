0e9d4e4318e174fe3194d28f0178afbe6ece454a
Bayu K. Luknanto <bayu.luknanto@tii.ae>
2026-09-09 13:47:50 +0700

    Remove unused parseArgs

--- src/app/startup/ArgsParser.cpp
+++ src/app/startup/ArgsParser.cpp
@@ -11,23 +11,6 @@
 
 namespace pendarlab::app::mavlink_hub::startup
 {
-  std::optional<Intent> parseArgs(int argc, char** argv, Intent intent)
-  {
-    CLI::App app{
-      "mavlink_hub is a tool that connects endpoints that uses Mavlink to agents (not AI agent!) so that agents can send and receive "
-      "mavlink messages via endpoints."
-    };
-
-    argv = app.ensure_utf8(argv);
-    app.add_option("-c,--config", intent.config_file_path, "Specifies which config file to use. Config file should be written in JSON");
-    try {
-      app.parse(argc, argv);
-    } catch (const CLI::ParseError& e) {
-      return std::nullopt;
-    }
-    return intent;
-  }
-
   std::optional<AppConfig> parseArgs(int argc, char** argv)
   {
     std::string config_file_path;
--- src/app/startup/ArgsParser.h
+++ src/app/startup/ArgsParser.h
@@ -8,6 +8,5 @@
 
 namespace pendarlab::app::mavlink_hub::startup
 {
-  std::optional<Intent> parseArgs(int argc, char** argv, Intent default_intent);
   std::optional<AppConfig> parseArgs(int argc, char** argv);
 } // namespace pendarlab::app::mavlink_hub::startup
