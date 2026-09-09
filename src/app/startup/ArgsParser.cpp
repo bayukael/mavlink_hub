d6ccc0b6d94576e39c22e3acf289c5586a060164
Bayu K. Luknanto <bayu.luknanto@tii.ae>
2026-09-09 11:28:56 +0700

    Make parseArgs overload that returns directly an AppConfig

--- src/app/startup/ArgsParser.cpp
+++ src/app/startup/ArgsParser.cpp
@@ -1,5 +1,6 @@
 #include "app/startup/ArgsParser.h"
 
+#include "app/types/AppConfig.h"
 #include "common/json_utils/JsonUtils.h"
 
 #include <cli11/CLI11.hpp>
@@ -27,4 +28,43 @@ namespace pendarlab::app::mavlink_hub::startup
     return intent;
   }
 
-} // namespace pendarlab::app::mavlink_hub
+  std::optional<AppConfig> parseArgs(int argc, char** argv)
+  {
+    std::string config_file_path;
+    CLI::App app{
+      "mavlink_hub is a tool that connects endpoints that uses Mavlink to agents (not AI agent!) so that agents can send and receive "
+      "mavlink messages via endpoints."
+    };
+
+    argv = app.ensure_utf8(argv);
+    app.add_option("-c,--config", config_file_path, "Specifies which config file to use. Config file should be written in JSON");
+
+    try {
+      app.parse(argc, argv);
+    } catch (const CLI::ParseError& e) {
+      return std::nullopt;
+    }
+
+    AppConfig app_cfg;
+    if(!config_file_path.empty()){
+      std::error_code ec;
+      std::string canonical_config_file_path = std::filesystem::canonical(config_file_path, ec);
+      if(ec){
+        return std::nullopt;
+      }
+
+      std::ifstream cfg_stream(canonical_config_file_path);
+      if (!cfg_stream.is_open()) {
+        return std::nullopt;
+      }
+
+      std::optional<AppConfig> processed_cfg = json_utils::fstreamToAppConfig(cfg_stream);
+      if (!processed_cfg.has_value()) {
+        return std::nullopt;
+      }
+      app_cfg = processed_cfg.value();
+    }
+    return app_cfg;
+  }
+
+} // namespace pendarlab::app::mavlink_hub::startup
--- src/app/startup/ArgsParser.h
+++ src/app/startup/ArgsParser.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "app/startup/Intent.h"
+#include "app/types/AppConfig.h"
 
 #include <optional>
 #include <string>
@@ -8,4 +9,5 @@
 namespace pendarlab::app::mavlink_hub::startup
 {
   std::optional<Intent> parseArgs(int argc, char** argv, Intent default_intent);
+  std::optional<AppConfig> parseArgs(int argc, char** argv);
 } // namespace pendarlab::app::mavlink_hub::startup
